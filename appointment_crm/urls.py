30820486b46b93be1eb1d60b620208a6038dba1c
Yago Brum Rodrigues <152230184+Yago-Brum@users.noreply.github.com>
2025-04-03 17:06:53 -0300

    Ajust my routes

--- appointment_crm/urls.py
+++ appointment_crm/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('crm.urls.api')),  # Rota para a API (clientes, agendamentos)
-    path('auth/', include('crm.urls.auth')),  # Rota para autenticação
+    path('API/', include('crm.urls.api')),  # Rota para a API (clientes, agendamentos)
+    path('API/auth/', include('crm.urls.auth')),  # Rota para autenticação
 ]
 
--- crm_frontend/src/axiosInstance.js
+++ crm_frontend/src/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,  // Usando a variável de ambiente
+  baseURL: process.env.REACT_APP_API_URL + '/API',  // Usando a variável de ambiente
   headers: {
     'Content-Type': 'application/json',
   },
